60b33ae79243875e0d6bbc42c211c5bfa27394cc
Hovhannes <hovhannespost@gmail.com>
2023-03-14 18:53:30 +0400

    fix java run stage

--- packages/frontend/src/app/app.jsx
+++ packages/frontend/src/app/app.jsx
@@ -1,6 +1,7 @@
 import NxWelcome from './nx-welcome';
 
 export function App() {
+  fetch('http://localhost:8080/api/addresses').then(console.log).catch(console.error);
   return (
     <>
       <NxWelcome title="frontend" />
